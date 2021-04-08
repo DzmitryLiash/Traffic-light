d578963b67b39c4ed0f157bd8efc3d4c84d50a28
Dzmitry Liashchou <liashchou.d@gmail.com>
2021-04-08 11:40:40 +0200

    Initial Commit

---
+++ Traffic light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic light
+//
+//  Created by Дмитрий Лещёв on 08/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
